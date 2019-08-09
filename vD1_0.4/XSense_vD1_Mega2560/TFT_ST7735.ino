fc96f2aee9fdc0d91702c9636386816be753a3ef
sensyncdev <48230878+sensyncdev@users.noreply.github.com>
2019-08-09 14:39:44 +0700

    Add files via upload

---
+++ vD1_0.4/XSense_vD1_Mega2560/TFT_ST7735.ino
@@ -0,0 +1,245 @@
+void displayStat(String teks){
+  tft.drawLine(0,110, tft.width(),110, ST77XX_WHITE);  
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setCursor(5, 115);  tft.println("Status:");
+  tft.fillRoundRect(45, 113, tft.width(), 123, 10, ST77XX_BLACK);
+  tft.setCursor(50,115);  tft.println(teks);
+}
+
+void displayTime() {
+  tft.fillRoundRect(0, 3, 99, 40, 10, ST77XX_BLACK);  
+  tft.setCursor(0, 0);
+  tft.setTextSize(3);
+  tft.setTextColor(ST77XX_WHITE);
+  
+  //-- display TIME  
+  tft.setCursor(5,5);
+  if(waktu.Hour<10){tft.print(F("0"));}
+  tft.print(waktu.Hour);
+  tft.print(F(":"));
+  if(waktu.Minute<10){tft.print(F("0"));}
+  tft.println(waktu.Minute);
+  
+  //-- display DATE  
+  tft.setTextSize(0);
+  tft.setTextColor(ST77XX_GREEN);
+  // tft.setCursor(5,35);
+  // tft.print(days[weekday-1]);
+  tft.setCursor(30,35);
+  if(waktu.Day<10){tft.print(F("0"));}
+  tft.print(waktu.Day);
+  tft.print(F("/"));
+  if(waktu.Month<10){tft.print(F("0"));}
+  tft.print(waktu.Month);
+  tft.print(F("/20"));
+  tft.print(waktu.Year);
+}
+
+void displayTempHumPres(){
+  //tft.fillRoundRect(100, 3, tft.width(), 43, 10, ST77XX_BLACK);  
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setCursor(100, 5);    tft.print(currentData.temp,1);
+  tft.println("C");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setCursor(100, 25);   tft.print("H:");
+  tft.setCursor(115, 25);   tft.print(currentData.rhum);
+  tft.println("%");
+  tft.setCursor(100, 35);   tft.print("P:");
+  tft.setCursor(115, 35);   tft.print(currentData.pressure,0);
+  tft.println("hPa");
+}
+
+void displayGasVal(){
+  int posY1=50, posY2=60;
+  int posX1=15, posX2=50, posX3=85, posX4=120;
+
+  tft.drawLine(0, 45, tft.width(), 45, ST77XX_WHITE);  
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_RED);
+  tft.setCursor(posX1, posY1);    tft.println("CO");
+  tft.setCursor(posX1-10,posY2);  tft.println(currentData.ppbCO);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setCursor(posX2, posY1);    tft.println("O3");
+  tft.setCursor(posX2-10,posY2);  tft.println(currentData.ppbO3);
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.setCursor(posX3, posY1);    tft.println("NO2");
+  tft.setCursor(posX3-10,posY2);  tft.println(currentData.ppbNO2);
+  tft.setTextColor(ST77XX_CYAN);
+  tft.setCursor(posX4, posY1);    tft.println("SO2");
+  tft.setCursor(posX4-10,posY2);  tft.println(currentData.ppbSO2);
+}
+
+void displayPMmeter(){
+  tft.drawLine(0, 70, tft.width(), 70, ST77XX_WHITE);  
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setCursor(5, 75);
+  tft.println(" PM1.0  PM2.5 PM4.0  PM10");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setCursor( 5, 85);   tft.print(currentData.pm1);
+  tft.setCursor(45, 85);   tft.print(currentData.pm25);
+  tft.setCursor(85, 85);   tft.print(currentData.pm4);
+  tft.setCursor(120,85);   tft.print(currentData.pm10);
+}
+
+void displayWind(){
+  int windSpd=(random(5,35)); 
+  int windDir=(random(10,360));
+  tft.drawLine(0,95, tft.width(),95, ST77XX_WHITE);  
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setCursor(5, 100);    tft.println("Wind:   km/h");
+  tft.setTextColor(ST77XX_CYAN);
+  tft.setCursor(40, 100);   tft.println(windSpd);
+  tft.setCursor(90, 100);   //tft.println(wDir);
+  DispDirection(windDir);
+}
+
+void DispDirection(int direction) {   // Display Wind Direction to LCD
+    // converts wind speed to wind strength    
+    if(direction < 22)
+      tft.print(F("NORTH"));
+    else if (direction < 67)
+      tft.print(F("NORTHEAST"));
+    else if (direction < 112)
+      tft.print(F("EAST"));
+    else if (direction < 157)
+      tft.print(F("SOUTHEAST"));
+    else if (direction < 212)
+      tft.print(F("SOUTH"));
+    else if (direction < 247)
+      tft.print(F("SOUTHWEST"));
+    else if (direction < 292)
+      tft.print(F("WEST"));
+    else if (direction < 337)
+      tft.print(F("NORTHWEST"));
+    else
+      tft.print(F("NORTH"));  
+}
+
+//---------------------------------------------------batas baru dan lama
+
+// void displaySetup() {
+//   tft.setTextWrap(false);
+//   tft.fillScreen(ST77XX_BLACK);
+//   tft.setTextColor(ST77XX_BLUE);
+//   tft.setTextSize(2);
+//   tft.setCursor(20, 20);
+//   tft.println("XSense");
+//   tft.setCursor(20, 50);
+//   tft.println("DGS SPEC");
+//   tft.setTextColor(ST77XX_YELLOW);
+//   tft.setCursor(35, 80);
+//   tft.setTextSize(1);
+//   tft.println("Sukabumi");
+//   delay(500);
+// }
+
+// void displayData() {
+//   Serial.println(F("Initializing ST7735S TFT..."));
+
+//   //data
+//   tft.setCursor(0, 0);
+//   tft.fillScreen(ST77XX_BLACK);
+//   tft.setCursor(5, 0);
+//   tft.setTextSize(0);
+//   String bulan;
+//   switch (waktuMonth) {
+//     case 1: bulan="JAN"; break;
+//     case 2: bulan="FEB"; break;
+//     case 3: bulan="MAR"; break;
+//     case 4: bulan="APR"; break;
+//     case 5: bulan="MAY"; break;
+//     case 6: bulan="JUN"; break;
+//     case 7: bulan="JUL"; break;
+//     case 8: bulan="AUG"; break;
+//     case 9: bulan="SEP"; break;
+//     case 10: bulan="OCT"; break;
+//     case 11: bulan="NOV"; break;
+//     case 12: bulan="DEC"; break;
+//   }
+//   String tanggal = waktuDay + ' ' + bulan + ' ' +  waktuYear;
+//   tft.println(tanggal); 
+//   tft.setCursor(5, 10);
+//   drawtextline("Starting...", ST77XX_WHITE, 1000);
+//   tft.setCursor(5, 20);
+//   drawtextline("Type HELP for command", ST77XX_WHITE, 1000);
+//   tft.setCursor(5, 30);
+//   drawtextline("Sampling...", ST77XX_WHITE, 1000);
+  
+//   tft.setCursor(5, 40);
+//   String displayTemp = "Temperature: " + String(currentData.temp,2) + " 'C";
+//   tft.println(displayTemp);
+  
+//   String displayRH = "Humidity: " + String(currentData.rhum,2) + " %";
+//   tft.println(displayRH);
+  
+//   String displayPressure = "Pressure: " + String(currentData.pressure,2) + " Pa";
+//   tft.println(displayPressure);
+
+//   String displayPM1 = "PM1.0: " + String(currentData.pm1,2) + " ug/mm3";
+//   tft.println(displayPM1);
+
+//   String displayPM25 = "PM2.5: " + String(currentData.pm25,2) + " ug/mm3";
+//   tft.println(displayPM25);
+
+//   String displayPM4 = "PM4.0: " + String(currentData.pm4,2) + " ug/mm3";
+//   tft.println(displayPM4);
+
+//   String displayPM10 = "PM10: " + String(currentData.pm10,2) + " ug/mm3";
+//   tft.println(displayPM10);
+
+//   String displayPPBCO = "Gas CO: " + String(currentData.ppbCO) + " ppb";
+//   tft.println(displayPPBCO);
+
+//   String displayPPBNO2 = "Gas NO2: " + String(currentData.ppbNO2) + " ppb";
+//   tft.println(displayPPBNO2);
+// }
+
+// //void TFTprintLogo(){
+// //  tft.setTextWrap(false);
+// //  tft.fillScreen(ST77XX_BLACK);
+// //  tft.setTextColor(ST77XX_BLUE);
+// //  tft.setTextSize(3);
+// //  tft.setCursor(20, 20);
+// //  tft.println("XSense");
+// //  tft.setCursor(45, 50);
+// //  tft.println("DGS SPEC");
+// //  tft.setTextColor(ST77XX_YELLOW);
+// //  tft.setCursor(35, 80);
+// //  tft.setTextSize(2);
+// //  tft.println("Sukabumi");
+// //  delay(3000);
+// //}
+// //
+// //void TFTprintBoot() {
+// //  tft.setCursor(0, 0);
+// //  tft.fillScreen(ST77XX_BLACK);
+// //  tft.setCursor(5, 0);
+// //  tft.setTextSize(0);
+// //  drawtextline("Senin, 10 Juni 2019", ST77XX_GREEN, 500);  
+// //  tft.setCursor(5, 10);
+// //  drawtextline("Starting...", ST77XX_WHITE, 1000);
+// //  tft.setCursor(5, 20);
+// //  drawtextline("Type HELP for command", ST77XX_WHITE, 1000);
+// //  tft.setCursor(5, 30);
+// //  drawtextline("Sampling...", ST77XX_WHITE, 1000);
+// //  tft.setCursor(5, 40);
+// //  drawtextline("Temp     : 29.64 'C", ST77XX_WHITE, 500);
+// //  tft.setCursor(5, 50);
+// //  drawtextline("Humidity : 54.67 %RH", ST77XX_WHITE, 500);
+// //  tft.setCursor(5, 60);
+// //  drawtextline("Pressure : 88120 Pa", ST77XX_WHITE, 500);
+// //  tft.setCursor(5, 70);
+// //  drawtextline("Int.Temp : 27.62 'C", ST77XX_WHITE, 500);
+// //  tft.setCursor(5, 80);
+// //  drawtextline("RTC Temp : 30.25 'C", ST77XX_WHITE, 500);
+// //  delay(3000);  
+// //}
+
+// void drawtextline(char *text, uint16_t color, int txtwait) {
+//   tft.setTextColor(color);
+//   tft.setTextWrap(false);
+//   tft.println(text);
+//   delay(txtwait);
+// }
---
+++ vD1_0.4/XSense_vD1_Mega2560/WeatherStation_WH24P.ino
@@ -0,0 +1,84 @@
+void get_wh24p() {
+    byte banyakbyte = 21;
+    byte bufferWH24P[banyakbyte];
+
+    Serial.print(F("\nTest WH24P. Sampling #")); Serial.println(1);
+
+    for(int z=0;z < banyakbyte;z++){bufferWH24P[z] = 0;}
+
+    //while((bufferWH24P[0] == 0) && (bufferWH24P[1] == 0) && (bufferWH24P[2] == 0)){
+    unsigned long startTime = millis();
+    while(millis() - startTime < 30000){
+      Serial2.readBytes(bufferWH24P,banyakbyte);
+      Serial.print(".");
+    }
+
+    Serial.println();
+    for(int z=0;z < banyakbyte;z++){
+        if(bufferWH24P[z] <= 15){Serial.print(0);}
+        Serial.print(bufferWH24P[z],HEX);
+        Serial.print(" ");
+    }
+
+    //--parsing
+    Serial.println();
+    Serial.print(bufferWH24P[2],HEX); Serial.print(" "); Serial.print(bufferWH24P[3],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.print(bufferWH24P[2],BIN); Serial.print(" "); Serial.println(bufferWH24P[3],BIN);
+    unsigned int winddir = (bufferWH24P[3] << 1) & 256; winddir = winddir | (bufferWH24P[2]); //-- D(256) = B(0001 0000 0000)
+    Serial.println(winddir,BIN);
+    Serial.print(F("Wind direction: ")); Serial.print(winddir); Serial.println(F(" (degree)"));
+
+    Serial.println();
+    Serial.print(bufferWH24P[3],HEX); Serial.print(" "); Serial.print(bufferWH24P[4],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.print(bufferWH24P[3],BIN); Serial.print(" "); Serial.println(bufferWH24P[4],BIN);
+    unsigned int tempzz = ((bufferWH24P[3] << 8) & 1792) | (bufferWH24P[4]); //-- D(1792) = B(0111 0000 0000)
+    Serial.println(tempzz,BIN);
+    //--rumus temperatur = (tempz-400)/10
+    float tempz = (tempzz-400)/10.0;  //in 'C
+    Serial.print(F("Temperature: ")); Serial.print(tempz); Serial.println(F(" (degree celsius)"));
+
+    Serial.println();
+    Serial.print(bufferWH24P[5],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.println(bufferWH24P[5],BIN);
+    byte humz = bufferWH24P[5]; //in %
+    Serial.print(F("Humidity: ")); Serial.print(humz); Serial.println(F(" (%)"));
+
+    Serial.println();
+    Serial.print(bufferWH24P[6],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.println(bufferWH24P[6],BIN);
+    unsigned int windspdz = (bufferWH24P[3] << 4) & 256; windspdz = windspdz | bufferWH24P[6]; //-- D(256) = B(0001 0000 0000)
+    Serial.println(windspdz,BIN);
+    float windspd = windspdz/0.14;  //in m/s
+    Serial.print(F("Wind speed: "));  Serial.print(windspd); Serial.println(F(" (m/s)"));
+
+    Serial.println();
+    Serial.print(bufferWH24P[7],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.println(bufferWH24P[7],BIN);
+    float gustspd = bufferWH24P[7]*1.12;  //in m/s
+    Serial.print(F("Gust speed: "));  Serial.print(gustspd); Serial.println(F(" (m/s)"));
+
+    Serial.println();
+    Serial.print(bufferWH24P[8],HEX); Serial.print(" "); Serial.print(bufferWH24P[9],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.print(bufferWH24P[8],BIN); Serial.print(" "); Serial.println(bufferWH24P[9],BIN);
+    unsigned int accrainfall = (bufferWH24P[8] << 8) | (bufferWH24P[9]); //in mm
+    Serial.println(accrainfall,BIN);
+    Serial.print(F("Accumulated rainfall: ")); Serial.print(accrainfall); Serial.println(F(" (mm)"));
+
+    Serial.println();
+    Serial.print(bufferWH24P[10],HEX); Serial.print(" "); Serial.print(bufferWH24P[11],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.print(bufferWH24P[10],BIN); Serial.print(" "); Serial.println(bufferWH24P[11],BIN);
+    unsigned int ultrav = (bufferWH24P[10] << 8) | (bufferWH24P[11]);  //in uW/cm2
+    Serial.println(ultrav,BIN);
+    Serial.print(ultrav); Serial.println(F(" (uW/cm2)"));
+
+    Serial.println();
+    Serial.print(bufferWH24P[12],HEX); Serial.print(" "); Serial.print(bufferWH24P[13],HEX); Serial.print(" "); Serial.print(bufferWH24P[14],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.print(bufferWH24P[12],BIN); Serial.print(" "); Serial.print(bufferWH24P[13],BIN); Serial.print(" "); Serial.println(bufferWH24P[14],BIN);
+    //unsigned long lightlux = 0; Serial.println(lightlux,BIN);  lightlux = (bufferWH24P[12] << 16) | (bufferWH24P[13] << 8); lightlux = (lightlux) | (bufferWH24P[14]);//in lux
+    unsigned long lightlux = 0; Serial.println(lightlux,BIN); 
+    lightlux = bufferWH24P[12]; lightlux = lightlux << 8; lightlux = lightlux | (bufferWH24P[13]); lightlux = lightlux << 8; lightlux = (lightlux) | (bufferWH24P[14]);//in lux
+    Serial.println(lightlux,BIN); Serial.println(lightlux,HEX);
+    float lightluxx = lightlux / 10.0;
+    Serial.print(lightluxx); Serial.println(F(" (lux)"));
+
+    Serial.println();
+    Serial.print(bufferWH24P[17],HEX); Serial.print(" "); Serial.print(bufferWH24P[18],HEX); Serial.print(" "); Serial.print(bufferWH24P[19],HEX); Serial.print(F(" <-HEX = BIN-> ")); Serial.print(bufferWH24P[17],BIN); Serial.print(" "); Serial.print(bufferWH24P[18],BIN); Serial.print(" "); Serial.println(bufferWH24P[19],BIN);
+    //unsigned long pressurexx = 0; Serial.println(pressurexx,BIN); pressurexx = (bufferWH24P[17] << 16) | (bufferWH24P[18] << 8); pressurexx = (pressurexx) | (bufferWH24P[19]); //in Pa
+    unsigned long pressurexx = 0; Serial.println(pressurexx,BIN);
+    pressurexx = bufferWH24P[17]; pressurexx = pressurexx << 8; pressurexx = pressurexx | bufferWH24P[18]; pressurexx = pressurexx << 8; pressurexx = (pressurexx) | (bufferWH24P[19]); //in Pa
+    Serial.println(pressurexx,BIN); Serial.println(pressurexx,HEX);
+    float pressurexz = pressurexx / 100.0;
+    Serial.print(pressurexz); Serial.println(F(" (hPa)"));
+}
